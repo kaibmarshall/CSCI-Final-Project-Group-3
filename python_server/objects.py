82bf6516558adeeae81aaadbbddab70db2237751
kaimarshall <kai.marshall@ucdenver.edu>
2021-12-08 13:15:13 -0700

    added python_server to project

---
+++ python_server/objects.py
@@ -0,0 +1,58 @@
+
+class Item:
+    def __init__(self, name :str, price_per_day :float):
+        self._name = name
+        self._price_per_day = price_per_day
+        self._is_available = True
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def price_per_day(self):
+        return self._price_per_day
+
+    @property
+    def is_available(self):
+        return self._is_available
+
+    @property
+    def set_availability(self, value :bool):
+        self._is_available = value
+
+
+class User:
+    def __init__(self, username :str, password :str, isAdmin :bool):
+        self._username = username
+        self._password = password
+        self._is_admin = isAdmin
+        self._rented_items = []
+
+    @property
+    def username(self):
+        return self._username
+
+    @property
+    def password(self):
+        return self._password
+
+    @property
+    def rented_items(self):
+        return self._rented_items
+
+    @property
+    def is_admin(self):
+        return self._is_admin
+
+    def rent_item(self, item :Item):
+        self._rented_items.append(item)
+
+    def return_item(self, item :Item):
+        if item in self._rented_items:
+            self._rented_items.remove(item)
+            return True
+        else:
+            return False
+
+
---
+++ python_server/server.py
@@ -0,0 +1,192 @@
+import socket
+from datetime import date
+from threading import Thread
+from objects import Item
+from objects import User
+class MultiServer:
+    def __init__(self, ip: str, port: int, backlog: int):
+        self.__ip = ip
+        self.__port = port
+        self.__backlog = backlog
+        self.__server_socket = None
+        self.__keep__running = True
+        self.__connection_count = 0
+        self.__list_cw = []
+
+        self._user_list = []
+        self._item_list = []
+
+
+    def terminate_server(self):
+        self.__keep__running = False
+        self.__server_socket.close()
+
+
+    def run(self):
+        self.__server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__server_socket.bind((self.__ip, self.__port))
+        self.__server_socket.listen(self.__backlog)
+
+        while self.__keep__running:
+            print(f"""[SRV] Waiting for Client""")
+            try:
+                client_socket, client_address = self.__server_socket.accept()
+                print(f"""[SRV] Got a connection from {client_address}""")
+                self.__connection_count +=1
+                cw = ClientWorker(self.__connection_count, client_socket, self)
+                self.__list_cw.append(cw)
+                cw.start()
+            except Exception as e:
+                print(e)
+
+        cw: ClientWorker
+        for cw in self.__list_cw:
+            cw.terminate_connection()
+            cw.join()
+
+    def send_message (self, msg:str, client_socket:socket):
+        print(f"""[SRV] send>> {msg}""")
+        client_socket.send(msg.encode())
+
+
+    def receive_message(self, client_socket:socket, max_length:int = 1024):
+        msg = client_socket.recv(max_length).decode()
+        print(f"""[SRV] RCV>> {msg}""")
+        return  msg
+
+    @property
+    def user_list(self):
+        return self._user_list
+
+
+class ClientWorker(Thread):
+    def __init__(self, client_id: int, client_socket: socket, server: MultiServer):
+        super().__init__()
+
+        self.__client_socket = client_socket
+        self.__keep_running_client = True
+        self.__server = server
+        self.__id = client_id
+
+
+    def run(self):
+        #self._send_message("Connected to Python Inventory Server")
+
+        while self.__keep_running_client:
+            self._process_client_request()
+
+        self.__client_socket.close()
+
+    def terminate_connection(self):
+        self.__keep_running_client = False
+        self.__client_socket.close()
+
+    def _display_message(self, message: str):
+        print(f"CLIENT >> {message}")
+
+    def _send_message (self, msg:str):
+        self._display_message(f"""SEND>> {msg}""")
+        self.__client_socket.send(msg.encode("UTF-8"))
+        pass
+
+    def _receive_message(self, max_length:int = 1024):
+        msg = self.__client_socket.recv(max_length).decode("UTF-8")
+        self._display_message(f"""[SRV] RCV>> {msg}""")
+        return msg
+
+
+    def _process_client_request(self):
+        client_message = self._receive_message()
+        self._display_message(f"CLIENT SAID>>>>{client_message}")
+
+        arguments = client_message.split("|")
+        response = ""
+
+        try:
+            if arguments[0] == "A":
+                response = "Item successfully added!\n"
+
+            elif arguments[0] == "N":
+                for user in self.__server.user_list:
+                    response += user._username + "|"
+
+            elif arguments[0] == "T":
+                self.terminate_connection()
+                response = "OK|T\n"
+
+            elif arguments[0] == "TERMINATE":
+                response = "OK\n"
+
+            else:
+                response = "ERR|Unknown Command\n"
+
+        except ValueError as ve:
+            response = "ERR|" + str(ve) + "\n"
+
+        self._send_message(response)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
